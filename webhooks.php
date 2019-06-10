579d175ab77a12ca9a232519ea8728af829efb2a
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-10 13:00:20 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,8 +16,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
-			$text .= $event['source']['userName'];
+			$text = $event['source']['userId'].' ';
+			$text .= date("ddMMYYYY").' ';
 			$text .= $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
