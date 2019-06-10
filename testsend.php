bd16d4fdbc001b6d6fee55c0bb855717c736023c
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-10 14:19:58 +0700

    Update testsend.php

--- testsend.php
+++ testsend.php
@@ -1,11 +1,17 @@
-<?php
-require "vendor/autoload.php";
-$access_token = 'la3nQ7rJ7FR8r0eTQPiYFfeE1vHNVIcdS4OoqTvWuRw/tGu9WG/joyP4vPOnQBqrv1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuVOwQrSKYZLYDck8hN/7PCuKeEWSlXfuR2iLyUTTRgovwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'e614b22719cb2e71e927dbd972d7249a';
-$idPush = 'U935fd498360db058d05404b3006cfa3a'
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage($idPush, $textMessageBuilder);
-
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+<html>
+	<head> 
+		<script>
+			$.ajax({ 
+			   type : "GET", 
+			   url : "https://api.line.me/v2/bot/message/10016075093981/content", 
+			   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer la3nQ7rJ7FR8r0eTQPiYFfeE1vHNVIcdS4OoqTvWuRw/tGu9WG/joyP4vPOnQBqrv1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuVOwQrSKYZLYDck8hN/7PCuKeEWSlXfuR2iLyUTTRgovwdB04t89/1O/w1cDnyilFU=');},
+			   success : function(result) { 
+			       //set your variable to the result 
+			   }, 
+			   error : function(result) { 
+			     //handle the error 
+			   } 
+			 }); 
+		</script>
+	</head>
+</html>
