cfd1233a688998e48bd537264bc46726f20166c3
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-10 15:17:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -20,7 +20,28 @@ if (!is_null($events['events'])) {
 			$text .= date('Y-m-d H:i:s').' ';
 			$text .= $event['message']['text'].' ';
 	
-			
+			$replyToken = $event['replyToken'];
+
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);	
 			
 
 			// Make a POST Request to Messaging API to reply to sender
