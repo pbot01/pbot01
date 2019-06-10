5290e0fdde08d9837590dc380627b4b18bf0c197
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-10 15:29:56 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -66,8 +66,7 @@ if (!is_null($events['events'])) {
 			if ($conn->connect_error) {
 			    die("Connection failed: " . $conn->connect_error);
 			} 
-			$sql = "INSERT INTO chatbot (time, user_id, message)
-			VALUES ('.$dt.', '.$uid.', '.$ms.')";
+			$sql = "INSERT INTO chatbot (user_id, message) VALUES ('".$uid."', '".$ms."')";
 			if ($conn->query($sql) === TRUE) {
 				
 				$text = "success";
