ef72f8089c04909ca79e309badedbf8620053a8f
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-13 22:40:19 +0700

    Update testgetuser.php

--- testgetuser.php
+++ testgetuser.php
@@ -5,7 +5,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 $access_token = 'HyW97ugLh5mEBYG+G2VCRbXrfqajv+kOrR+uHtkyItGUABVj7AfJ3+gp8j0VykAov1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuUkTrYUWIzQBqf8v39IGKVW3G3+wwZ3xEAgcPgggMGNyAdB04t89/1O/w1cDnyilFU=';
 
-$url = "https://api.line.me/v2/bot/profile/U935fd498360db058d05404b3006cfa3a";
+$url = "https://api.line.me/v2/bot/message/10036084556718/content";
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
