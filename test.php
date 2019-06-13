94201e5c3f0d973e2e470887dfc9a61dd7153176
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-14 00:34:42 +0700

    Update test.php

--- test.php
+++ test.php
@@ -8,7 +8,7 @@ require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 $access_token = 'HyW97ugLh5mEBYG+G2VCRbXrfqajv+kOrR+uHtkyItGUABVj7AfJ3+gp8j0VykAov1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuUkTrYUWIzQBqf8v39IGKVW3G3+wwZ3xEAgcPgggMGNyAdB04t89/1O/w1cDnyilFU=';
 
 
-				$url = "https://api.line.me/v2/bot/group/Cd9d6f02742c9c16b1bb293b13817611f/members/ids";
+				$url = "https://api.line.me/v2/bot/group/Cd9d6f02742c9c16b1bb293b13817611f";
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 				$ch = curl_init($url);
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
