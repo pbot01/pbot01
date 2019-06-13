deb8d0c40603a8dcadab7d7e792a7fc6806a3eae
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-14 01:12:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -93,6 +93,87 @@ if (!is_null($events['events'])) {
 			}
 			$conn->close();
 		}
+		else if ($event['type'] == 'message' && $event['message']['type'] == 'location') {
+			// Get text sent
+			
+			$uid = $event['source']['userId'];
+			$gid = $event['source']['groupId'];
+			$dt= date('Y-m-d H:i:s');
+			$ms= $event['message']['title']." ".$event['message']['address'];
+			$lo= "latitude:".$event['message']['latitude']." longitude:".$event['message']['longitude'];
+			
+			$text = $event['source']['userId'].' ';
+			$text .= date('Y-m-d H:i:s').' ';
+			$text .= $event['message']['text'].' ';
+			$text = $content;
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+
+			$post = json_encode($data);
+		
+			// reply message	
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			//get display name
+			
+			$url = "https://api.line.me/v2/bot/profile/".$uid;
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			$result = curl_exec($ch);
+			//echo $result;
+			$profile =  json_decode($result, true); 
+			$disname = $profile['displayName'];
+			curl_close($ch);
+			
+			
+
+			echo $result . "\r\n";
+			
+			$servername = "pbot01.hopto.org";
+			$username = "research_usr";
+			$password = "123456789";
+			$dbname = "pbot01";
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			mysqli_set_charset($conn,"utf8");
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			} 
+			$sql = "INSERT INTO chatbot (message_type,time,user_id,message,group_id,displayname,location) VALUES ('text',SYSDATE(),'".$uid."', '".$ms."', '".$gid."', '".$disname."', '".$lo."')";
+			if ($conn->query($sql) === TRUE) {
+				
+				$text = "success";
+			} else {
+			    	$text = "fail";
+			}
+			$conn->close();
+		}
 		else {
 						// Get text sent
 			$uid = $event['source']['userId'];
