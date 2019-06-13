ac6833439a105e185f44a80adccb717d87b4a922
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-13 23:16:13 +0700

    Update testgetuser.php

--- testgetuser.php
+++ testgetuser.php
@@ -7,26 +7,28 @@ $access_token = 'HyW97ugLh5mEBYG+G2VCRbXrfqajv+kOrR+uHtkyItGUABVj7AfJ3+gp8j0VykA
 
 $url = "https://api.line.me/v2/bot/message/10036084556718/content";
 			$headers = array('Content-Type: image/jpeg', 'Authorization: Bearer ' . $access_token);
-// 			$ch = curl_init($url);
-// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// 			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// 			$result = curl_exec($ch);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+header('Content-type: image/jpeg');
+echo $result;
 // $profile =  json_decode($result, true); 
 // 			$disname = $profile['displayName'];
 //The cURL stuff...
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
-$picture = curl_exec($ch);
-curl_close($ch);
-//Display the image in the browser
-header('Content-type: image/jpeg');
-echo $picture;
+// $ch = curl_init($url);
+// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+// $picture = curl_exec($ch);
+// curl_close($ch);
+// //Display the image in the browser
+// header('Content-type: image/jpeg');
+// echo $picture;
 
 
 			curl_close($ch);
