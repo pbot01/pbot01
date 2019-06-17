f3ac84a87be53d244e201c4e3ee11117d37dc42e
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-17 17:16:37 +0700

    Update showdata.php

--- showdata.php
+++ showdata.php
@@ -5,14 +5,6 @@
 
 		<?php // callback.php
 
-		$id = $_GET["no"];
-		header('Content-type:text/html; charset=utf-8');
-		require "vendor/autoload.php";
-		require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
-		$access_token = 'HyW97ugLh5mEBYG+G2VCRbXrfqajv+kOrR+uHtkyItGUABVj7AfJ3+gp8j0VykAov1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuUkTrYUWIzQBqf8v39IGKVW3G3+wwZ3xEAgcPgggMGNyAdB04t89/1O/w1cDnyilFU=';
-
-
 					$servername = "pbot01.hopto.org";
 					$username = "research_usr";
 					$password = "123456789";
