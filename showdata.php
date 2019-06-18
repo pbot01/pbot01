a45aca1466119c5ce4e95d86807f71bbe878548b
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-18 09:03:53 +0700

    Update showdata.php

--- showdata.php
+++ showdata.php
@@ -110,6 +110,22 @@
 								$conn->close();
 					?>
 			  </tbody>
+			  <tfoot>
+			    <tr>
+			      <th>Name
+			      </th>
+			      <th>Position
+			      </th>
+			      <th>Office
+			      </th>
+			      <th>Age
+			      </th>
+			      <th>Start date
+			      </th>
+			      <th>Salary
+			      </th>
+			    </tr>
+			  </tfoot>
 			</table>
 		</div>
 	</body>
