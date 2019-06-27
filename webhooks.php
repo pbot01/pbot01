37084564ca1a5837a742b0f7b9a5c57bb18071f1
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-27 13:29:25 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@ header('Content-type:text/html; charset=utf-8');
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'HyW97ugLh5mEBYG+G2VCRbXrfqajv+kOrR+uHtkyItGUABVj7AfJ3+gp8j0VykAov1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuUkTrYUWIzQBqf8v39IGKVW3G3+wwZ3xEAgcPgggMGNyAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'iLDkHbjueMjxmKu3rbb09lc1z3mu95S93R2Hr00mrm/fyvLdmAU9egAUIrCcCbnjv1yoQeM09Zpft/LYPj4FcqF0fEHWXLbqzb1gaTZSxuVLv13g6rIQYs6KLGW/Aph+LatpMjqDrZKLS8UUjsFCHQdB04t89/1O/w1cDnyilFU=';
 $servername = "pbot001.cuicotomxesg.us-east-2.rds.amazonaws.com";
 $username = "pbot001";
 $password = "123456789";
