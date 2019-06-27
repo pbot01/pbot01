4d94e0830f10f2f6b8d3de391fcc0135ac493709
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-27 13:44:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -49,7 +49,7 @@ if (!is_null($events['events'])) {
 			// Create connection
 			$conn = new mysqli($servername, $username, $password, $dbname);
 			// Check connection
-			mysqli_set_charset($conn,"utf8");
+			//mysqli_set_charset($conn,"utf8");
 			if ($conn->connect_error) {
 			    die("Connection failed: " . $conn->connect_error);
 				$text .= "connection error ";
