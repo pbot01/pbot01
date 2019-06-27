d3c4b5d4e17b2905476072ae601d3767ff61137e
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-06-27 13:26:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -76,10 +76,41 @@ if (!is_null($events['events'])) {
 
 			
 
-			$text = $disname;
+			$text = $profile;
 			
 			//$replyToken = $event['replyToken'];
 
+			// Build message to reply back
+
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+
+			$post = json_encode($data);
+		
+			// reply message	
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			
+			
+			echo $result . "\r\n";
+			
 			// Create connection
 			$conn = new mysqli($servername, $username, $password, $dbname);
 			// Check connection
