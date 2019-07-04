31d1feb67ba4d4c7b8c4f36980b218a03dab1ee3
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-07-04 10:22:02 +0700

    Update register.php

--- register.php
+++ register.php
@@ -35,13 +35,6 @@
 						Registoration
 					</span>
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="name" placeholder="Firstname-Lastname">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
-						</span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
