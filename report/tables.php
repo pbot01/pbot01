659505279e657cc313cba92765af8a5a4eb4897d
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-07-11 08:50:20 +0700

    Update tables.php

--- report/tables.php
+++ report/tables.php
@@ -157,7 +157,7 @@
           </button>
 
           <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" method="GET" action="./table.php" >
             <div class="input-group">
               <input type="date" class="form-control bg-light border-0 small" placeholder="Date..." aria-label="Search" aria-describedby="basic-addon2">
               <div class="input-group-append">
