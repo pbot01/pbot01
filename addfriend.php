e524d3926dbe4148c5c54cd99fc67dfae659d0e3
pbot01 <51575465+pbot01@users.noreply.github.com>
2019-07-11 13:41:11 +0700

    Create addfriend.php

---
+++ addfriend.php
@@ -0,0 +1,7 @@
+<html>
+<head></head>
+<body>
+<div class="line-it-button" data-lang="en" data-type="friend" data-lineid="@022brbja" style="display: none;"></div>
+ <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+</doby>
+</html>
